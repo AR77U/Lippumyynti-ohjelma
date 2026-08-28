3ab5c2cc90a30a46df99ddae15e1d65745e2af6d
4Apila <tituzka@hotmail.com>
2026-08-28 16:16:22 +0300

    Initialized the Springboot project

---
+++ lipunmyynti/src/test/java/lipunmyynti/lipunmyynti/LipunmyyntiApplicationTests.java
@@ -0,0 +1,13 @@
+package lipunmyynti.lipunmyynti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LipunmyyntiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
